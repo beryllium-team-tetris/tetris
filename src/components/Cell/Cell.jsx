01b0334bb474a38bdc4bcc2cf91bc52d2ff5d0da
Andrea Cleland <andreaclelan@gmail.com>
2022-05-27 10:23:00 -0700

    added commas to tetrominos which fixed error

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyledCell } from '../../Styles/StyledCell';
-// import { TETROMINOS } from '../../utils/tetrominos';
+import { StyledCell } from '../Styles/StyledCell';
+import { TETROMINOS } from '../../utils/tetrominos';
 
 export default function Cell({ type }) {
   return (
-    <StyledCell type={type} color={'red'}>
+    <StyledCell type={type} color={TETROMINOS['I'].color}>
       Cell
     </StyledCell>
   );
