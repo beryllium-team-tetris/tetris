7f1d581bb4038ce74cd80e33eec91f386a8b3cad
Andrea Cleland <andreaclelan@gmail.com>
2022-06-01 09:57:09 -0700

    updated App.jsx to include routes for scores and profiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Switch, Route } from 'react-router-dom';
 import Auth from './views/Auth/Auth';
 import Tetris from './views/Tetris/Tetris';
-import ScoreBoard from './views/Scoreboard/Scoreboard';
+import Leaderboard from './views/Scores/Leaderboard';
+import ScoreDetail from './views/Scores/ScoreDetail';
 
 export default function App() {
   return (
@@ -10,9 +11,24 @@ export default function App() {
         <Route path="/login">
           <Auth />
         </Route>
-        <Route path="/scores">
-          <ScoreBoard />
+
+        <Route path="/scores/:id">
+          <ScoreDetail />
         </Route>
+        <Route path="/scores">
+          <Leaderboard />
+        </Route>
+
+        {/* <Route path="/profile/create">
+          <CreateProfile />
+        </Route>
+        <Route path="/profile/:id/edit">
+          <ProfileEdit />
+        </Route>
+        <Route path="/profile/:id">
+          <Profile />
+        </Route> */}
+
         <Route path="/">
           <Tetris />
         </Route>
