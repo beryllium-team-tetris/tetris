2980ba5a6db4a78103890750e7bd6fbcc2101630
Cole Rossman <colerossman99@gmail.com>
2022-05-31 17:24:04 -0700

    user.js hooks created for logout, signup, login, as well ass providing access to rest of user state

--- src/hooks/user.js
+++ src/hooks/user.js
@@ -9,4 +9,27 @@ export const useAuth = () => {
     if (context === undefined) {
         throw new Error('useAuth must be used within a useProvider');
     }
+
+    const { user, setUser, email, setEmail, password, setPassword, error, setError, type, setType, currentUser } = context;
+
+    const isLoggedIn = user?.email;
+
+    const login = async (email, password) => {
+        const authenticatedUser = await signInUser(email, password);
+        setUser(authenticatedUser);
+    };
+
+    const signUp = async (email, password) => {
+        const newUser = await signUpUser(email, password);
+        setUser(newUser);
+    };
+
+    const logout = () => {
+        setUser({ email: null });
+        signOutUser();
+    };
+
+    return {
+        user, isLoggedIn, signUp, login, logout, email, setEmail, password, setPassword, error, setError, type, setType, currentUser
+    };
 }
