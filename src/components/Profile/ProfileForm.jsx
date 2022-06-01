133056bb9a87b41035fd9ad7a3bb60634c40f605
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-01 12:39:35 -0700

    added profiles, edits and form

---
+++ src/components/Profile/ProfileForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function ProfileForm({
+  username,
+  setUsername,
+  email,
+  setEmail,
+  name,
+  setName,
+}) {
+  return (
+    <form>
+      <label>
+        Username
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Email
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Name
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+
+      <button onClick={handleSubmit}>Profile</button>
+    </form>
+  );
+}
