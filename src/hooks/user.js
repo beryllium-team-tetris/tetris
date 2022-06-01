76977b094cb8d598e494ff6b1595321858d2d472
Andrea Cleland <andreaclelan@gmail.com>
2022-06-01 15:23:45 -0700

    linked sign up and create profile logic

--- src/hooks/user.js
+++ src/hooks/user.js
@@ -1,35 +1,60 @@
-import { useContext } from "react";
-import { UserContext } from "../context/UserContext";
-import { signUpUser, signInUser, signOutUser } from "../services/users";
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+import { signUpUser, signInUser, signOutUser } from '../services/users';
 
 export const useAuth = () => {
+  const context = useContext(UserContext);
 
-    const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within a useProvider');
+  }
 
-    if (context === undefined) {
-        throw new Error('useAuth must be used within a useProvider');
-    }
+  const {
+    user,
+    setUser,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    error,
+    setError,
+    type,
+    setType,
+    currentUser,
+  } = context;
 
-    const { user, setUser, email, setEmail, password, setPassword, error, setError, type, setType, currentUser } = context;
+  const isLoggedIn = user?.email;
 
-    const isLoggedIn = user?.email;
+  const login = async (email, password) => {
+    const authenticatedUser = await signInUser(email, password);
+    setUser(authenticatedUser);
+  };
 
-    const login = async (email, password) => {
-        const authenticatedUser = await signInUser(email, password);
-        setUser(authenticatedUser);
-    };
+  const signUp = async (email, password) => {
+    const newUser = await signUpUser(email, password);
+    setUser(newUser);
+    return newUser;
+  };
 
-    const signUp = async (email, password) => {
-        const newUser = await signUpUser(email, password);
-        setUser(newUser);
-    };
+  const logout = () => {
+    setUser({ email: null });
+    signOutUser();
+  };
 
-    const logout = () => {
-        setUser({ email: null });
-        signOutUser();
-    };
-
-    return {
-        user, isLoggedIn, signUp, login, logout, email, setEmail, password, setPassword, error, setError, type, setType, currentUser
-    };
-}
+  return {
+    user,
+    isLoggedIn,
+    signUp,
+    login,
+    logout,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    error,
+    setError,
+    type,
+    setType,
+    currentUser,
+  };
+};
--- src/services/users.js
+++ src/services/users.js
@@ -1,25 +1,34 @@
 import { client } from './client';
+import { createProfile } from './profile';
 
 export function getUser() {
-    return client.auth.user();
+  return client.auth.user();
 }
 
 export function getSession() {
-    return client.auth.session();
+  return client.auth.session();
 }
 
 export async function signUpUser(email, password) {
-    const { user, error } = await client.auth.signUp({ email, password });
-    if (error) throw error;
-    return user;
+  const { user, error } = await client.auth.signUp({ email, password });
+  console.log('user resp', user);
+  const profile_resp = await createProfile({
+    email,
+    user_id: user.id,
+    username: '',
+    name: '',
+  });
+  console.log('profile resp', profile_resp);
+  if (error) throw error;
+  return user;
 }
 
 export async function signInUser(email, password) {
-    const { user, error } = await client.auth.signIn({ email, password });
-    if (error) throw error;
-    return user;
+  const { user, error } = await client.auth.signIn({ email, password });
+  if (error) throw error;
+  return user;
 }
 
 export async function signOutUser() {
-    return client.auth.signOut();
+  return client.auth.signOut();
 }
