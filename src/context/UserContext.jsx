0df168193e3c9289abfdd5d22c58ae6ac4fbf92e
Cole Rossman <colerossman99@gmail.com>
2022-05-31 17:08:52 -0700

    UserContext created and all user state now accessable

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,1 +1,33 @@
 import { createContext, useState } from "react";
+import { getUser } from "../services/users";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    
+    const currentUser = getUser();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [type, setType] = useState('sign-up');
+    const [user, setUser] = useState(currentUser || {email: null});
+
+    return (
+        <UserContext.Provider value={{
+            user,
+            setUser,
+            email,
+            setEmail,
+            password,
+            setPassword,
+            error,
+            setError,
+            type,
+            setType,
+            currentUser,
+        }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
