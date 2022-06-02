dcb19aedb3c70280c5e3650e76cfde2b3b5fef9e
Andrea Cleland <andreaclelan@gmail.com>
2022-06-02 16:06:30 -0700

    styled leaderboard

---
+++ src/components/Styles/StyledLeaderboard.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const StyledLeaderboard = styled.div`
+  width: 100vw;
+  height: 100vw;
+  background-color: rgb(0, 0, 0);
+  background-size: cover;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledScore = styled.td`
+  color: green;
+`;
+
+export const StyledUsername = styled.td`
+  color: blueviolet;
+`;
+
+export const StyledHeading = styled.h1`
+  color: red;
+`;
--- src/views/Scores/Leaderboard.jsx
+++ src/views/Scores/Leaderboard.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { StyledTetrisWrapper } from '../../components/Styles/StyledTetris';
 import { useScores } from '../../hooks/scores';
 import { getScores } from '../../services/scores';
 import { Link } from 'react-router-dom';
+import {
+  StyledHeading,
+  StyledLeaderboard,
+  StyledScore,
+  StyledUsername,
+} from '../../components/Styles/StyledLeaderboard';
 
 export default function Leaderboard() {
   const { scores } = useScores();
   const topScores = scores.slice(0, 10);
   return (
-    <StyledTetrisWrapper>
-      <h1>Leaderboard</h1>
+    <StyledLeaderboard>
+      <StyledHeading>Leaderboard</StyledHeading>
 
       <table>
         <thead>
@@ -18,19 +23,21 @@ export default function Leaderboard() {
             <th>Score</th>
           </tr>
         </thead>
-        {topScores.map((score) => (
-          <tr key={score.id}>
-            <td>
-              <Link to={`/profile/${score.profile_id}`}>
-                {score.profiles.username
-                  ? score.profiles.username
-                  : score.profiles.email}
-              </Link>
-            </td>
-            <td>{score.score}</td>
-          </tr>
-        ))}
+        <tbody>
+          {topScores.map((score) => (
+            <tr key={score.id}>
+              <StyledUsername className="username">
+                <Link to={`/profile/${score.profile_id}`}>
+                  {score.profiles.username
+                    ? score.profiles.username
+                    : score.profiles.email}
+                </Link>
+              </StyledUsername>
+              <StyledScore className="score">{score.score}</StyledScore>
+            </tr>
+          ))}
+        </tbody>
       </table>
-    </StyledTetrisWrapper>
+    </StyledLeaderboard>
   );
 }
