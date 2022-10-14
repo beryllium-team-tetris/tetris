4f43e1727195cb612f6ba3a77ffd0a904d5a3a42
Andrea Cleland <andreaclelan@gmail.com>
2022-10-14 13:18:37 -0700

    began work on pause feature

--- src/components/StartButton/StartButton.jsx
+++ src/components/StartButton/StartButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyledStartButton } from '../Styles/StyledStartButton';
 
-export default function StartButton({ callback }) {
+export default function StartButton({ callback, paused=false }) {
   return <StyledStartButton onClick={callback}>Start</StyledStartButton>;
 }
